c9b7b47c048c35f2ca1c4de8aef85a0edba92f47
W_C__L <18842685026@163.com>
2016-08-16 22:44:26 +0800

    Initial Commit

---
+++ WCLLoadingView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WCLLoadingView
+//
+//  Created by 王崇磊 on 16/8/16.
+//  Copyright © 2016年 王崇磊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
